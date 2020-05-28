6e0c8f37f069fc530a78ff1d07108a5e4d0ce630
Illinium <khodorko88@gmail.com>
2020-05-28 18:08:28 +0300

    28.05_from Oleksii

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,30 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    posts: []
   },
   mutations: {
+    updatePosts(state, posts) {
+      state.posts = posts;
+    }
   },
   actions: {
+    async fetchPosts(ctx, limit = 3) {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/posts?_limit=${limit}`
+      );
+      const posts = await res.json();
+      ctx.commit('updatePosts', posts);
+    }
   },
-  modules: {
+  modules: {},
+  getters: {
+    allPosts(state) {
+      return state.posts;
+    }
   }
-})
+});
--- src/views/List.vue
+++ src/views/List.vue
@@ -1,15 +1,22 @@
 <template>
-  <div>
-      <h1>List</h1>  
-  </div>  
+  <div id="app">
+    <div class="list-wrapper" v-for="post in allPosts" :key="post.id">
+      <div>{{ post.id }}</div>
+      <div>{{ post.title }}</div>
+      <div>{{ post.body }}</div>
+    </div>
+  </div>
 </template>
 
 <script>
+import { mapGetters, mapActions } from 'vuex';
+import './List.css';
 export default {
-
-}
+  name: 'app',
+  computed: mapGetters(['allPosts']),
+  methods: mapActions(['fetchPosts']),
+  async mounted() {
+    this.fetchPosts();
+  }
+};
 </script>
-
-<style>
-
-</style>
